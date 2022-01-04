666f9cb0f0b891be88f26677c84da79e9ab328a4
UtkarshA135 <utkarshagarwal101@gmail.com>
2022-01-04 22:59:49 +0530

    major

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-const map = L.map('map', {zoomControl: false}).setView([0.051774, 37.905596], 7);
+const map = L.map('map', {zoomControl: false}).setView([12.9716, 77.5946], 7);
 
 const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
